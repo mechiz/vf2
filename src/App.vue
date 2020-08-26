45bd51627f08f5418f47b514e99086ad30544433
mechiz <mechiz@naver.com>
2020-08-26 16:20:31 +0900

    title 수정을 다시해

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      jklgklj
+      title 수정을 다시해
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
